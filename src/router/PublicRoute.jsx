be0f4c10415c3ad4afedce11485ed008ae751712
StevenDev <bossiolealbl@gmail.com>
2024-06-27 17:40:49 -0500

    Configuracion de rutas publicas

---
+++ src/router/PublicRoute.jsx
@@ -0,0 +1,10 @@
+import React, { Children, useContext } from 'react'
+import { AuthContext } from '../auth'
+import { Navigate } from 'react-router-dom';
+
+export const PublicRoute = ({children}) => {
+    const {authState} = useContext(AuthContext);
+  return (authState.logged)
+    ? <Navigate to='/' />
+    : children
+}
