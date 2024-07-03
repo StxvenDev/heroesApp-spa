2fa89afb28726accd1436872c4e94d780a93a92e
StevenDev <bossiolealbl@gmail.com>
2024-07-03 17:15:27 -0500

    test: init second test

--- test/ui/components/Navbar.test.jsx
+++ test/ui/components/Navbar.test.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Navbar } from "../../../src/ui";
 import { AuthContext } from '../../../src/auth';
 import { MemoryRouter } from 'react-router-dom';
 
 /**
- * Todo Nombre de la persona
- * Todo cuando hace click 
+ * Todo: Nombre de la persona
+ * Todo: cuando hace click en el boton "logout" debe llamar 
  *  * 1. navigate con arg 'login' y replace
  *  * 2. logout
  */
@@ -31,4 +31,24 @@ describe('test in <Navbar />', () => {
         const userName = screen.getByLabelText('span').innerHTML;
         expect(userName).toBe(authState.user.name);
      });
+     test('should call navigate and logout function', () => { 
+        const logout = jest.fn();
+        const authState = {
+            logged : true,
+            user : {
+                id : 'abc',
+                name : 'Juan'
+            }
+        }
+        render(
+            <MemoryRouter initialEntries={['/marvel']}>
+                <AuthContext.Provider value={{authState, logout}}>
+                    <Navbar />
+                </AuthContext.Provider>
+            </MemoryRouter>
+        );
+        const buttonLogout = screen.getByRole('button');
+        fireEvent.click(buttonLogout);
+        expect(logout).toHaveBeenCalled();
+      });
  });
